6be7e5927656a3aee7b5d54a0cb4d4bc6e8d2e0f
vateseif <vateseif@gmail.com>
2023-09-18 02:01:43 +0200

    Add obstacle

--- panda_gym/envs/panda_tasks/panda_move_table.py
+++ panda_gym/envs/panda_tasks/panda_move_table.py
@@ -18,7 +18,7 @@ class PandaMoveTable(RobotTaskEnv):
 
     def __init__(self, render: bool = False, reward_type: str = "sparse", control_type: str = "ee") -> None:
         sim = PyBullet(render=render)
-        robot1 = Panda(sim, block_gripper=False, base_position=np.array([-0.8, 0.0, 0.0]), base_orientation=np.array([0, 0, -0.5]), control_type=control_type, body_name="_left")
+        robot1 = Panda(sim, block_gripper=False, base_position=np.array([-0.5, 0.0, 0.0]), base_orientation=np.array([0, 0, -0.5]), control_type=control_type, body_name="_left")
         robot2 = Panda(sim, block_gripper=False, base_position=np.array([0.5, 0.0, 0.0]), base_orientation=np.array([0, 0, 2.6]), control_type=control_type, body_name="_right", base_gripper_orientation=np.array([np.pi,0.,np.pi]))
         robots = [robot1, robot2]
         task = MoveTable(sim, reward_type=reward_type)
